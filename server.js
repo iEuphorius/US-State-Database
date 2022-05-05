2c7e3147ad32cf55f00ffe1c8e20175aa3de196a
Taylor <tamynhier@mail.fhsu.edu>
2022-05-05 19:36:58 -0400

    changed cors

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 3500;
 connectDB();
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors());
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+});
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
